96b926f503a3b3f630acf45e36f0abeebc1887ba
krysnuvadga <tetechris20@gmail.com>
2020-02-28 07:12:09 -0500

    all

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$configuration = array(
+    'db_dsn'  => 'mysql:host=localhost;dbname=shipbattle',
+    'db_user' => 'root',
+    'db_pass' => '',
+);
